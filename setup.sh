fe258efcc9ac2c713aeebabfa70b279257ee32a6
Douglas Strodtman <douglas.strodtman@databricks.com>
2019-07-02 18:06:54 -0700

    fixing aliases

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 docker build -t db-cli .
 
-if grep -e ". "$PWD"/alias.sh" ~/.bash_profile; then
-  echo "Using alias at" $PWD
-else 
-  echo ". "$PWD"/alias.sh" >>~/.bash_profile
-  . ~/.bash_profile
-  echo "Alias set"
-fi
+# if grep -e ". "$PWD"/alias.sh" ~/.bash_profile; then
+#   echo "Using alias at" $PWD
+# else 
+#   echo ". "$PWD"/alias.sh" >>~/.bash_profile
+# 
+#   echo "Alias set"
+# fi
+
+. alias.sh
